6930016585ad3ca4e3b31e3cf4cd411be06a6294
luis.guerra <luis.guerra@neoris.com>
2019-10-17 09:47:16 -0500

    Comparisons

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FaroShuffle.Extensions;
+
+namespace FaroShuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            var startingDeck = from s in Suits()
+                               from r in Ranks()
+                               select new { Suit = s, Rank = r };
+            //var startingDeck = from s in Suits()          from r in Ranks()      select new { Suit = s, Rank = r };
+            //var startingDeck = Suits().SelectMany(suit => Ranks().Select(rank => new { Suit = suit, Rank = rank }));                    
+
+            var times = 0;
+            var shuffle = startingDeck;
+            do
+            {
+                shuffle = shuffle.Take(26).InterleaveSequenceWith(shuffle.Skip(26));
+           
+                foreach (var card in shuffle)
+                {
+                    Console.WriteLine(card);
+                }
+                Console.WriteLine();
+                times++;
+
+             }while (!startingDeck.SequenceEquals(shuffle));
+
+
+            Console.WriteLine(times);
+
+
+
+
+        }
+
+
+        static IEnumerable<string> Suits()
+        {
+            yield return "clubs";
+            yield return "diamonds";
+            yield return "hearts";
+            yield return "spades";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "two";
+            yield return "three";
+            yield return "four";
+            yield return "five";
+            yield return "six";
+            yield return "seven";
+            yield return "eight";
+            yield return "nine";
+            yield return "ten";
+            yield return "jack";
+            yield return "queen";
+            yield return "king";
+            yield return "ace";
+        }
+
+    }
+}
